908858ac52a69d48d155384ede0dd3134f36bcfb
clemilton <clemilton.fucapi@gmail.com>
2021-07-12 19:44:55 -0400

    02 - TextInputLayout

--- app/src/main/java/com/clemilton/instagram/login/prensentation/LoginActivity.java
+++ app/src/main/java/com/clemilton/instagram/login/prensentation/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.clemilton.instagram.login.prensentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.clemilton.instagram.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        TextInputLayout inputLayoutTest = findViewById(R.id.login_edit_text_email_input);
+       inputLayoutTest.setError("Esse email é inválido!");
+
+        EditText editText = findViewById(R.id.login_edit_text_email);
+        editText.setBackground(ContextCompat.getDrawable(LoginActivity.this,
+                R.drawable.edit_text_background_error));
     }
 }
