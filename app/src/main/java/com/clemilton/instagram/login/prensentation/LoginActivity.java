a5222c7fafa18f1bd68185dad8f1b711df534177
clemilton <clemilton.fucapi@gmail.com>
2021-07-12 13:52:43 -0400

    01 - Estilos tela login

--- app/src/main/java/com/clemilton/instagram/login/prensentation/LoginActivity.java
+++ app/src/main/java/com/clemilton/instagram/login/prensentation/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.clemilton.instagram;
+package com.clemilton.instagram.login.prensentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.clemilton.instagram.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
